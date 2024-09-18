623073434dd284ba36b6653add548da3918d04d0
bhanutejaindla <87014662+bhanutejaindla@users.noreply.github.com>
2024-09-19 00:34:33 +0530

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
         };
 
         try {
-            const response = await fetch('http://localhost:3001/login', {
+            const response = await fetch('https://admin-server-8syd.onrender.com/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
